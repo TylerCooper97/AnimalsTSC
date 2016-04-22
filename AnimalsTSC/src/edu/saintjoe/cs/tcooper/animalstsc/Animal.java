1f15d1c9523d5dc8bd91e9c2ab6d11fc703c94ef
tcooper <tcooper@localhost>
2016-04-22 19:18:51 -0400

    Overloading practice

--- AnimalsTSC/src/edu/saintjoe/cs/tcooper/animalstsc/Animal.java
+++ AnimalsTSC/src/edu/saintjoe/cs/tcooper/animalstsc/Animal.java
@@ -4,8 +4,36 @@ public class Animal {
 	
 	String picture;
 	String location;
+	int weight;
+	String foodType;
+	int age;
 	
 	/*---------------------*/
+	public Animal() {
+		//this is a constructor
+		//even though we didn't write this before it was always here by default
+		this.location = "nowhere";
+		this.picture = "no picture";
+	}
+	
+	public Animal (String startingLocation) {
+		this.location = startingLocation;
+	}
+	
+	public Animal (int startingWeight){
+		this.weight = startingWeight;
+	}
+	
+	public Animal (String foodType, int age){
+		this.foodType = foodType;
+		this.age = age;
+	}
+	public Animal (String startLocation, String startPicture, int startWeight){
+		this.weight = startWeight;
+		this.location = startLocation;
+		this.picture = startPicture;
+	}
+	
 	
 	public String makeNoise(){
 		return("Ommmmmmmmmmmmmmmm");
--- AnimalsTSC/src/edu/saintjoe/cs/tcooper/animalstsc/AnimalTester.java
+++ AnimalsTSC/src/edu/saintjoe/cs/tcooper/animalstsc/AnimalTester.java
@@ -3,15 +3,20 @@ package edu.saintjoe.cs.tcooper.animalstsc;
 public class AnimalTester {
 
 	public static void main(String[] args) {
-		Dog C = new Dog();
-		Dog Taco = new Chihuahua();
-		Cat Garfield = new Cat();
-		Reptile Hiss = new Snake();
-		
-		System.out.println(C.makeNoise());
-		System.out.println(Taco.makeNoise());
-		System.out.println(Garfield.makeNoise());
-		System.out.println(Hiss.makeNoise());
+	Animal myPet = new Animal();//this instantiation calls the default constructor
+	Animal myLocalPet = new Animal("Here");//this one uses the one parameter constructor
+	Animal Billy = new Animal(20);
+	Animal Alfred = new Animal("My house", "Dog", 25);
+	Animal Jimmy = new Animal("Beef", 15);
+	
+	System.out.println("myPet has " + myPet.location + " as its location");
+	System.out.println("myLocalPet has " + myLocalPet.location + " as its location");
+	System.out.println("Billy has " + Billy.weight + " as its weight");
+	System.out.println("Alfred has " + Alfred.weight + " as its weight");
+	System.out.println("Alfred has " + Alfred.location + " as its location");
+	System.out.println("Alfred has " + Alfred.picture + " as its picture");
+	System.out.println("Jimmy has " + Jimmy.foodType + " as its food type");
+	System.out.println("Jimmy has " + Jimmy.age + " as its age");
 
 	}
 
